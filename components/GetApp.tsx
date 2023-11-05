48b8be48c3ccd00dffe6068b76b1b5d7cd39633c
Yantasticc <yashkalamkhede2002@gmail.com>
2023-11-05 22:30:39 +0530

    hero complete

---
+++ components/GetApp.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GetApp = () => {
+  return (
+    <div>
+      getapp
+    </div>
+  )
+}
+
+export default GetApp
---
+++ components/Guide.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Guide = () => {
+  return (
+    <div>
+      guide
+    </div>
+  )
+}
+
+export default Guide
